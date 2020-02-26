2102e8a34e837f3b6db9a691a004d69b3599c0bb
bamke25 <nurullahke25@gmail.com>
2020-02-26 14:05:42 +0700

    Create belajar_helper.php

---
+++ E41180169_Nur/belajar_ci/application/helpers/belajar_helper.php
@@ -0,0 +1,11 @@
+<?php
+function tampilkanTebal ($val) {//menampilkan teks tebal
+echo '<br>' . $val . '</b>';
+}
+function tampilkanMiring ($val) {//menampilakn teks miring
+echo '<i>' . $val . '</i>';
+}
+function tampilkanBergaris ($val) {//menampilakan teks bergaris
+echo '<u>' . $val . '</u>';
+}
+?>
