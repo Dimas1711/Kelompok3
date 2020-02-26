705df1e3854c4bb26e7800a9e67b706f81d57566
Ryan Hartadi <Ryanhartadi06@gmail.com>
2020-02-26 21:56:23 +0700

    tugas 1

---
+++ E41180111 Ryan Hartadi/codeigniter/application/helpers/belajar_helper.php
@@ -0,0 +1,13 @@
+<?php 
+
+function tampilkanTebal($val){
+    echo '<b>' . $val . '</b>';
+}
+function tampilkanMiring($val){
+    echo '<i>' . $val . '</i>';
+}
+function tampilkanBergaris($val){
+    echo '<u>' . $val . '</u>';
+}
+
+?>
---
+++ E41180111 Ryan Hartadi/codeigniter/application/views/HomeView.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar CodeIgniter</title>
+</head>
+<body>
+    <h1>Masukkan Data Diri Anda</h1>
+    <!-- <p>
+            Berikut ini adalah mahasiswa santuy:
+    </p>
+    Nama : <?php echo $data ["nama"];?> <br>
+    Status : <?php echo $data ["status"];?><br>
+    Website : <?php echo $data ["website"];?> --> 
+
+    <?php 
+    echo $error;
+    if ($data) {
+        ?>
+        <h3>Gambar berhasil Di Upload</h3>
+        <img src="../gambar/<?php echo $data ["file_name"];?>" width="200">
+   <?php 
+    }
+    ?>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="gambar" id="gambar"><br>
+        <button type="submit">Kirim</button>
+    </form>
+</body>
+</html>
