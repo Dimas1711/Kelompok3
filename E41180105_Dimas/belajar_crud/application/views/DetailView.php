d0f1f0e100ba90333ec2b48631959e3abcb31a5d
Dimas1711 <pdimas429@gmail.com>
2020-03-05 07:39:06 +0700

    crud

---
+++ E41180105_Dimas/belajar_crud/application/views/DetailView.php
@@ -0,0 +1,6 @@
+<h1><?php echo $artikel[0]->judul;?></h1>
+<h3>Tanggal : <?php echo $artikel[0]->tanggal;?> | Penulis : <?php echo $artikel[0]->penulis;?></h3>
+<div>
+    <?php echo $artikel[0]->isi;?>
+</div>
+<a href="../../../">Kembali</a>
